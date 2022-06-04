fb8c92a3917eb3c0d564bf65a3ace43a60fcbcdf
ogu anthony <oguanthony@ogus-MacBook-Pro.local>
2022-06-04 15:50:58 +0100

    face detection

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.lenght) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not Found')
+            }
+        }) 
+        .catch(err => res.status(400).json('error getting user'))
+    }
+
+    module.exports = {
+        handleProfileGet
+    }
